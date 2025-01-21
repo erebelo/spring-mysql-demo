5bc1a486d5bcca6951a306f2806c6aa149cfb202
erebelo <eduldrebelo@gmail.com>
2025-01-21 18:12:51 -0300

    Removing LoggerFactory references

--- src/main/java/com/erebelo/springmysqldemo/exception/GlobalExceptionHandler.java
+++ src/main/java/com/erebelo/springmysqldemo/exception/GlobalExceptionHandler.java
@@ -6,8 +6,7 @@ import com.erebelo.springmysqldemo.exception.model.UnprocessableEntityException;
 import jakarta.validation.ConstraintViolationException;
 import java.sql.SQLException;
 import java.util.List;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.dao.InvalidDataAccessApiUsageException;
@@ -24,29 +23,28 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
-
-    private static final Logger LOGGER = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
     @ResponseBody
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ExceptionResponse> handleException(Exception exception) {
-        LOGGER.error("Exception thrown:", exception);
+        log.error("Exception thrown:", exception);
         return parseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
     }
 
     @ResponseBody
     @ExceptionHandler(IllegalStateException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalStateException(IllegalStateException exception) {
-        LOGGER.error("IllegalStateException thrown:", exception);
+        log.error("IllegalStateException thrown:", exception);
         return parseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
     }
 
     @ResponseBody
     @ExceptionHandler(IllegalArgumentException.class)
     public ResponseEntity<ExceptionResponse> handleIllegalArgumentException(IllegalArgumentException exception) {
-        LOGGER.error("IllegalArgumentException thrown:", exception);
+        log.error("IllegalArgumentException thrown:", exception);
         return parseExceptionMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
@@ -54,7 +52,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(ConstraintViolationException.class)
     public ResponseEntity<ExceptionResponse> handleConstraintViolationException(
             ConstraintViolationException exception) {
-        LOGGER.error("ConstraintViolationException thrown:", exception);
+        log.error("ConstraintViolationException thrown:", exception);
         return parseExceptionMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
@@ -62,7 +60,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMediaTypeNotSupportedException.class)
     public ResponseEntity<ExceptionResponse> handleHttpMediaTypeNotSupportedException(
             HttpMediaTypeNotSupportedException exception) {
-        LOGGER.error("HttpMediaTypeNotSupportedException thrown:", exception);
+        log.error("HttpMediaTypeNotSupportedException thrown:", exception);
         return parseExceptionMessage(HttpStatus.UNSUPPORTED_MEDIA_TYPE, exception.getMessage());
     }
 
@@ -70,7 +68,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpMessageNotReadableException.class)
     public ResponseEntity<ExceptionResponse> handleHttpMessageNotReadableException(
             HttpMessageNotReadableException exception) {
-        LOGGER.error("HttpMessageNotReadableException thrown:", exception);
+        log.error("HttpMessageNotReadableException thrown:", exception);
         return parseExceptionMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
@@ -78,7 +76,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
     public ResponseEntity<ExceptionResponse> handleHttpRequestMethodNotSupportedException(
             HttpRequestMethodNotSupportedException exception) {
-        LOGGER.error("HttpRequestMethodNotSupportedException thrown:", exception);
+        log.error("HttpRequestMethodNotSupportedException thrown:", exception);
 
         String errorMessage = exception.getMessage();
         var supportedHttpMethods = exception.getSupportedMethods();
@@ -93,7 +91,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ExceptionResponse> handleMethodArgumentNotValidException(
             MethodArgumentNotValidException exception) {
-        LOGGER.error("MethodArgumentNotValidException thrown:", exception);
+        log.error("MethodArgumentNotValidException thrown:", exception);
 
         String errorMessage = null;
         List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
@@ -106,7 +104,7 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(TransactionSystemException.class)
     public ResponseEntity<ExceptionResponse> handleTransactionSystemException(TransactionSystemException exception) {
-        LOGGER.error("TransactionSystemException thrown:", exception);
+        log.error("TransactionSystemException thrown:", exception);
 
         var errorMessage = "An error occurred during transaction processing";
         var rootCause = exception.getRootCause();
@@ -120,45 +118,45 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DataIntegrityViolationException.class)
     public ResponseEntity<ExceptionResponse> handleDataIntegrityViolationException(
             DataIntegrityViolationException exception) {
-        LOGGER.error("DataIntegrityViolationException thrown:", exception);
+        log.error("DataIntegrityViolationException thrown:", exception);
         return parseExceptionMessage(HttpStatus.CONFLICT, exception.getRootCause().getMessage());
     }
 
     @ExceptionHandler(DuplicateKeyException.class)
     public ResponseEntity<ExceptionResponse> handleDuplicateKeyException(DuplicateKeyException exception) {
-        LOGGER.error("DuplicateKeyException thrown:", exception);
+        log.error("DuplicateKeyException thrown:", exception);
         return parseExceptionMessage(HttpStatus.CONFLICT, exception.getMessage());
     }
 
     @ExceptionHandler(SQLException.class)
     public ResponseEntity<ExceptionResponse> handleSQLException(SQLException exception) {
-        LOGGER.error("SQLException thrown:", exception);
+        log.error("SQLException thrown:", exception);
         return parseExceptionMessage(HttpStatus.INTERNAL_SERVER_ERROR, exception.getMessage());
     }
 
     @ExceptionHandler(InvalidDataAccessApiUsageException.class)
     public ResponseEntity<ExceptionResponse> handleInvalidDataAccessApiUsageException(
             InvalidDataAccessApiUsageException exception) {
-        LOGGER.error("InvalidDataAccessApiUsageException thrown:", exception);
+        log.error("InvalidDataAccessApiUsageException thrown:", exception);
         return parseExceptionMessage(HttpStatus.BAD_REQUEST, exception.getMessage());
     }
 
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ExceptionResponse> handleNotFoundException(NotFoundException exception) {
-        LOGGER.error("NotFoundException thrown:", exception);
+        log.error("NotFoundException thrown:", exception);
         return parseExceptionMessage(HttpStatus.NOT_FOUND, exception.getMessage());
     }
 
     @ExceptionHandler(ConflictException.class)
     public ResponseEntity<ExceptionResponse> handleConflictException(ConflictException exception) {
-        LOGGER.error("ConflictException thrown:", exception);
+        log.error("ConflictException thrown:", exception);
         return parseExceptionMessage(HttpStatus.CONFLICT, exception.getMessage());
     }
 
     @ExceptionHandler(UnprocessableEntityException.class)
     public ResponseEntity<ExceptionResponse> handleUnprocessableEntityException(
             UnprocessableEntityException exception) {
-        LOGGER.error("UnprocessableEntityException thrown:", exception);
+        log.error("UnprocessableEntityException thrown:", exception);
         return parseExceptionMessage(HttpStatus.UNPROCESSABLE_ENTITY, exception.getMessage());
     }
 
